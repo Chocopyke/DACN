d464f266e766ec14bc0afadf40183cbc36b54f69
Chocopyke <Vu.tuanson159@gmail.com>
2024-10-25 10:45:13 +0700

    Update vpc modules

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,120 @@
+#Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-vpc"
+  }
+}
+
+#Get all availability zones
+data "aws_availability_zones" "available_zones" {}
+
+#Create Public Subnet for VPC
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-public-subnet"
+  }
+}
+
+#Create Private Subnet for VPC
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-private-subnet"
+  }
+}
+
+#Create Internet Gateway for VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-igw"
+  }
+}
+
+#Create Public Route Table and routing rules, then connect it to Public Subnet
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rtb.id
+}
+
+#Create NAT Gateway for VPC
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-eip"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-ngw"
+  }
+}
+
+#Create Private Route Table and routing rules, then connect it to Private Subnet
+resource "aws_route_table" "private_rtb" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "${var.proj_name}-${var.environment}-private-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rtb.id
+}
+
+#Create Default Security Groups for VPC
+resource "aws_security_group" "default-sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "environment" {}
+variable "proj_name" {}
+
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
