5ab2d268bacd4f2ab585f307c953b6370d50f583
Chocopyke <Vu.tuanson159@gmail.com>
2024-11-22 15:41:52 +0700

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,86 +10,86 @@ module "vpc" {
   private_subnet_cidr = "192.168.2.0/24"
 }
 
-# module "ecr" {
-#   source          = "./modules/ecr"
-#   repository_name = "lamlt-sonvt"
-# }
+module "ecr" {
+  source          = "./modules/ecr"
+  repository_name = "lamlt-sonvt"
+}
 
-# module "ecs_frontend" {
-#   source             = "./modules/ecs"
-#   region             = "us-east-1"
-#   cluster_name       = "dacn-cluster"
-#   task_family        = "dacn-frontend"
-#   cpu                = "512"
-#   memory             = "1024"
-#   container_name     = "front-end"
-#   image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:front-end"
-#   container_port     = 5173
-#   host_port          = 5173
-#   protocol           = "tcp"
-#   service_name       = "front-end"
-#   desired_count      = 1
-#   subnet_ids         = [module.vpc.private_subnet_id]
-#   security_group_ids = [module.vpc.ecs_sg_id]
-#   assign_public_ip   = true
-# }
+module "ecs_frontend" {
+  source             = "./modules/ecs"
+  region             = "us-east-1"
+  cluster_name       = "dacn-cluster"
+  task_family        = "dacn-frontend"
+  cpu                = "512"
+  memory             = "1024"
+  container_name     = "front-end"
+  image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:front-end"
+  container_port     = 5173
+  host_port          = 5173
+  protocol           = "tcp"
+  service_name       = "front-end"
+  desired_count      = 1
+  subnet_ids         = [module.vpc.private_subnet_id]
+  security_group_ids = [module.vpc.ecs_sg_id]
+  assign_public_ip   = true
+}
 
-# module "ecs_backend_cart" {
-#   source             = "./modules/ecs"
-#   region             = "us-east-1"
-#   cluster_name       = "dacn-cluster"
-#   task_family        = "dacn-backend-cart"
-#   cpu                = "512"
-#   memory             = "1024"
-#   container_name     = "cart"
-#   image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:cart"
-#   container_port     = 3003
-#   host_port          = 3003
-#   protocol           = "http"
-#   service_name       = "cart"
-#   desired_count      = 1
-#   subnet_ids         = [module.vpc.private_subnet_id]
-#   security_group_ids = [module.vpc.ecs_sg_id]
-#   assign_public_ip   = false
-# }
+module "ecs_backend_cart" {
+  source             = "./modules/ecs"
+  region             = "us-east-1"
+  cluster_name       = "dacn-cluster"
+  task_family        = "dacn-backend-cart"
+  cpu                = "512"
+  memory             = "1024"
+  container_name     = "cart"
+  image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:cart"
+  container_port     = 3003
+  host_port          = 3003
+  protocol           = "http"
+  service_name       = "cart"
+  desired_count      = 1
+  subnet_ids         = [module.vpc.private_subnet_id]
+  security_group_ids = [module.vpc.ecs_sg_id]
+  assign_public_ip   = false
+}
 
-# module "ecs_backend_product" {
-#   source             = "./modules/ecs"
-#   region             = "us-east-1"
-#   cluster_name       = "dacn-cluster"
-#   task_family        = "dacn-backend-product"
-#   cpu                = "512"
-#   memory             = "1024"
-#   container_name     = "product"
-#   image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:product"
-#   container_port     = 3002
-#   host_port          = 3002
-#   protocol           = "http"
-#   service_name       = "product"
-#   desired_count      = 1
-#   subnet_ids         = [module.vpc.private_subnet_id]
-#   security_group_ids = [module.vpc.ecs_sg_id]
-#   assign_public_ip   = false
-# }
+module "ecs_backend_product" {
+  source             = "./modules/ecs"
+  region             = "us-east-1"
+  cluster_name       = "dacn-cluster"
+  task_family        = "dacn-backend-product"
+  cpu                = "512"
+  memory             = "1024"
+  container_name     = "product"
+  image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:product"
+  container_port     = 3002
+  host_port          = 3002
+  protocol           = "http"
+  service_name       = "product"
+  desired_count      = 1
+  subnet_ids         = [module.vpc.private_subnet_id]
+  security_group_ids = [module.vpc.ecs_sg_id]
+  assign_public_ip   = false
+}
 
-# module "ecs_backend_user" {
-#   source             = "./modules/ecs"
-#   region             = "us-east-1"
-#   cluster_name       = "dacn-cluster"
-#   task_family        = "dacn-backend-user"
-#   cpu                = "512"
-#   memory             = "1024"
-#   container_name     = "user"
-#   image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:user"
-#   container_port     = 3001
-#   host_port          = 3001
-#   protocol           = "http"
-#   service_name       = "user"
-#   desired_count      = 1
-#   subnet_ids         = [module.vpc.private_subnet_id]
-#   security_group_ids = [module.vpc.ecs_sg_id]
-#   assign_public_ip   = false
-# }
+module "ecs_backend_user" {
+  source             = "./modules/ecs"
+  region             = "us-east-1"
+  cluster_name       = "dacn-cluster"
+  task_family        = "dacn-backend-user"
+  cpu                = "512"
+  memory             = "1024"
+  container_name     = "user"
+  image              = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:user"
+  container_port     = 3001
+  host_port          = 3001
+  protocol           = "http"
+  service_name       = "user"
+  desired_count      = 1
+  subnet_ids         = [module.vpc.private_subnet_id]
+  security_group_ids = [module.vpc.ecs_sg_id]
+  assign_public_ip   = false
+}
 
 # module "ecs_sonarqube" {
 #   source             = "./modules/ecs"
