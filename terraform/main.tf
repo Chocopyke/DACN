0fb1e87caa810905a9e571c52ddacc50ce36bd75
Chocopyke <Vu.tuanson159@gmail.com>
2024-12-20 10:21:02 +0700

    many changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,8 @@ module "route53_private_hosted_zone" {
   source = "./modules/route53"
   dns_name = "backend.local"
   vpc_id = module.vpc.vpc_id
-  alias_name = module.alb.internal_alb_dns_name
-  alias_zone_id = module.alb.internal_alb_zone_id
+  alias_name = module.alb.internet_alb_dns_name
+  alias_zone_id = module.alb.internet_alb_zone_id
   evaluate_target_health = false
 }
 
@@ -26,42 +26,7 @@ module "route53_private_hosted_zone" {
 #   repository_name = "lamlt-sonvt"
 # }
 
-# module "ecs-frontend" {
-#   source       = "./modules/ecs"
-#   cluster_name = "dacn-cluster"
-#   family       = "dacn-task-family"
-#   cpu          = "512"
-#   memory       = "1024"
-
-#   container_name    = "front-end"
-#   image             = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:web"
-#   container_port    = 5173
-#   host_port         = 5173
-#   service_name      = "front-end-service"
-#   desired_count     = 1
-#   subnets           = [module.vpc.private_subnet_1_id]
-#   security_group_id = [module.vpc.dacn_sg_id]
-#   assign_public_ip  = false
-
-#   target_group_arn = module.alb.target_group_arn
-# }
-
-# module "ecs-product" {
-#   source       = "./modules/ecs"
-#   cluster_name = "dacn-cluster"
-#   family       = "dacn-task-family"
-#   cpu          = "512"
-#   memory       = "1024"
-
-#   container_name    = "product-service"
-#   image             = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:product"
-#   container_port    = 3002
-#   host_port         = 3002
-#   service_name      = "product-service"
-#   desired_count     = 1
-#   subnets           = [module.vpc.private_subnet_1_id]
-#   security_group_id = [module.vpc.dacn_sg_id]
-#   assign_public_ip  = false
-
-#   target_group_arn = module.alb.target_group_arn
-# }
+module "ecs" {
+  source = "./modules/ecs"
+  cluster_name = "dacn-cluster"
+}
--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,45 +1,103 @@
 resource "aws_ecs_cluster" "this" {
   name = var.cluster_name
 }
-# Tạo ECS Task Definition
-resource "aws_ecs_task_definition" "this" {
-  family                   = var.family
-  network_mode             = "awsvpc"
+
+
+#Create ECS Task Definiton
+resource "aws_ecs_task_definition" "front_end_task" {
+  family = "front-end"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = var.cpu
-  memory                   = var.memory
-  execution_role_arn       = "arn:aws:iam::329599660036:role/ecsTaskExecutionRole"
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  task_role_arn = "arn:aws:iam::329599660036:role/ecsTaskRole"
+  execution_role_arn = "arn:aws:iam::329599660036:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
-      name      = var.container_name
-      image     = var.image
+      name      = "front-end"
+      image     = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:front-end"
+      cpu       = 256
+      memory    = 512
       essential = true
       portMappings = [
         {
-          containerPort = var.container_port
-          hostPort      = var.host_port
+          containerPort = 5173
+          hostPort      = 5173
         }
       ]
     }
   ])
 }
-# Tạo ECS Service
-resource "aws_ecs_service" "this" {
-  name            = var.service_name
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = var.desired_count
-  launch_type     = "FARGATE"
 
-  network_configuration {
-    subnets          = var.subnets
-    security_groups  = var.security_group_id
-    assign_public_ip = var.assign_public_ip
-  }
-
-  load_balancer {
-    target_group_arn = var.target_group_arn
-    container_name   = var.container_name
-    container_port   = var.container_port
-  }
+resource "aws_ecs_task_definition" "product_service_task" {
+  family = "product-service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  task_role_arn = "arn:aws:iam::329599660036:role/ecsTaskRole"
+  execution_role_arn = "arn:aws:iam::329599660036:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+      name      = "product"
+      image     = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:product"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3002
+          hostPort      = 3002
+        }
+      ]
+    }
+  ])
+}
+resource "aws_ecs_task_definition" "cart_service_task" {
+  family = "cart-service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  task_role_arn = "arn:aws:iam::329599660036:role/ecsTaskRole"
+  execution_role_arn = "arn:aws:iam::329599660036:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+      name      = "cart"
+      image     = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:cart"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3003  
+          hostPort      = 3003
+        }
+      ]
+    }
+  ])
+}
+resource "aws_ecs_task_definition" "user_service_task" {
+  family = "user-service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  task_role_arn = "arn:aws:iam::329599660036:role/ecsTaskRole"
+  execution_role_arn = "arn:aws:iam::329599660036:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+      name      = "user"
+      image     = "329599660036.dkr.ecr.us-east-1.amazonaws.com/lamlt-sonvt:user"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3001
+          hostPort      = 3001
+        }
+      ]
+    }
+  ])
 }
